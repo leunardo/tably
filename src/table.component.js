11ddb2de8b79cb869a6390c9cedb52aeb4436729
Leonardo de Melo Alves <leunardosevla@gmail.com>
2019-08-01 00:38:40 -0300

    feat: add render methods

--- src/table.component.js
+++ src/table.component.js
@@ -7,12 +7,43 @@ export class TableComponent extends LitElement {
         super();
     }
 
-    @property()
-    name = 'World';
+    @property({type: Array})
+    columns = [];
+
+    @property({type: Array})
+    rows = [];
 
     render() {
         return html`
-            <p>Hello, ${this.name}!</p>
+            <header>${renderColumns(this.columns)}</header>
+            <main>${renderRows(this.rows, this.columns)}</main>
+            <footer>
+                <slot name="tably-footer"></slot>
+            </footer>
         `
     }
+}
+
+function resolveDisplayValue(value, column) {
+    if (!column || typeof column.displayWith !== 'function') {
+        return value;
+    }
+
+    return column.displayWith(value);
+}
+
+function renderColumns(columns) {
+    return columns.map(c => html`
+        <slot name="${c.prop}"></slot>
+    `);
+}
+
+function renderRows(rows, columns) {
+    return rows.map(row => {
+        return columns.map(column => renderCell(column, row));
+    });
+}
+
+function renderCell(column, row) {
+    return html`<slot slot-value="${resolveDisplayValue(row[column.prop], column)}" name="tably-cell-${column.prop}">${resolveDisplayValue(row[column.prop], column)}</slot>`
 }
