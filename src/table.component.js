277c2b513ee6ee22e4f5083c76a19c5647f2a5ea
Leonardo de Melo Alves <leunardosevla@gmail.com>
2019-08-03 11:25:24 -0300

    feat: rewrite markup
    markup rewritten as table
    added horizontal scroll property
    added mobile view

--- src/table.component.js
+++ src/table.component.js
@@ -3,8 +3,11 @@ import {LitElement, html, customElement, property} from 'lit-element';
 @customElement('tably-component')
 export class TableComponent extends LitElement {
 
+
     constructor() {
         super();
+        renderRows = renderRows.bind(this);
+        renderCell = renderCell.bind(this);
     }
 
     @property({type: Array})
@@ -13,14 +16,90 @@ export class TableComponent extends LitElement {
     @property({type: Array})
     rows = [];
 
+    @property({type: Array})
+    mobileConstraintWidth = 600;
+
+    @property({type: Boolean})
+    horizontalScroll;
+
+    get styles () {
+        return html`<style>
+            table {
+                border-collapse: collapse;
+                border: 1px solid #ccc;
+                width: 100%;
+            }
+            
+            table tr {
+                background-color: #f8f8f8;
+                border: 1px solid #ddd;
+                padding: .35em;
+            }
+            
+            table th,
+            table td {
+                padding: .625em;
+                text-align: center;
+            }
+            
+            main {
+                overflow-x: ${this.horizontalScroll ? 'auto' : 'inherit'};
+            }
+
+            @media screen and (max-width: ${this.mobileConstraintWidth + 'px'})  {
+                table {
+                    border: 0;
+                }
+                table thead {
+                    border: none;
+                    display: none;
+                    overflow: hidden;
+                    position: absolute; 
+                }
+                table tr {
+                    border-bottom: 3px solid #ddd;
+                    display: block;
+                    margin-bottom: .625em;
+                }
+                table td {
+                    border-bottom: 1px solid #ddd;
+                    display: block;
+                    font-size: .8em;
+                    text-align: right;
+                }
+                table td::before {
+                    content: attr(data-label);
+                    float: left;
+                    font-weight: bold;
+                    text-transform: uppercase;
+                }
+                table td:last-child {
+                    border-bottom: 0;
+                }
+                main {
+                    overflow-x: inherit;
+                }
+            }
+        </style>`;
+    }
+
     render() {
         return html`
-            <header>${renderColumns(this.columns)}</header>
-            <main>${renderRows(this.rows, this.columns)}</main>
-            <footer>
-                <slot name="tably-footer"></slot>
-            </footer>
-        `
+            ${this.styles}
+            <main class="tably-wrapper">
+                <table>
+                    <thead>
+                        <tr>${renderColumns(this.columns)}</tr>
+                    </thead>
+                    <tbody>
+                      ${renderRows(this.rows, this.columns)}  
+                    </tbody>
+                    <tfoot>
+                        <slot name="tably-footer"></slot>
+                    </tfoot>
+                </table>
+            </main>
+        `;
     }
 }
 
@@ -34,16 +113,23 @@ function resolveDisplayValue(value, column) {
 
 function renderColumns(columns) {
     return columns.map(c => html`
-        <slot name="${c.prop}"></slot>
+        <th class="tably-col">
+            <slot name="${c.prop}">${c.name}</slot>
+        </th>
     `);
 }
 
 function renderRows(rows, columns) {
-    return rows.map(row => {
-        return columns.map(column => renderCell(column, row));
-    });
+    return rows.map(row => html`
+        <tr class="tably-row">
+            ${columns.map(column => renderCell(column, row))}
+        </tr>
+    `);
 }
 
 function renderCell(column, row) {
-    return html`<slot slot-value="${resolveDisplayValue(row[column.prop], column)}" name="tably-cell-${column.prop}">${resolveDisplayValue(row[column.prop], column)}</slot>`
+    return html`
+        <td data-label="${column.name}" class="tably-cell">
+            ${resolveDisplayValue(row[column.prop], column)}
+        </td>`;
 }
